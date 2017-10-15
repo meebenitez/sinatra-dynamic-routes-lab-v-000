b708fe6392f30a142dba2b277ad0e3477484d196
Mary Benitez <meebenitez@gmail.com>
2017-10-15 06:27:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
     "#{world_1} " + "#{world_2} " + "#{world_3} " + "#{world_4} " + "#{world_5}."
   end
 
-  get "/:operation/:number1/:number2" do 
+  get "/:operation/:number1/:number2" do
     operator = params[:operation]
     num1 = params[:number1]
     num2 = params[:number2]
