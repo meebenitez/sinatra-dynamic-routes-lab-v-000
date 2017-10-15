463b408a345498a6139fa5e4b777fac8d317bff2
Mary Benitez <meebenitez@gmail.com>
2017-10-15 05:54:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ class App < Sinatra::Base
   get "/reversename/:name" do
     forward_name = params[:name]
     backward_name = forward_name.reverse
-    "Hello #{backward_name}!"
+    "#{backward_name}"
 
 end
