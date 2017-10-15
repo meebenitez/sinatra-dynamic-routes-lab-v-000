0518cbefa408e246848971ac2839ba4296220f97
Mary Benitez <meebenitez@gmail.com>
2017-10-15 05:52:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
 
 end
